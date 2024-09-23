58bde76d0cdbb264b9f9a9fe1ca791734253bce9
Derricknjaramba <njarambaderrick@gmail.com>
2024-09-23 11:24:29 +0300

    add2

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
